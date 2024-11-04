049a33f27760cc2763332f0312be2e137a652325
Toshario sams <shariosams07@gmail.com>
2024-11-04 18:45:05 -0500

    newest

---
+++ practice/01_return.js
@@ -0,0 +1,38 @@
+
+// #1 Complete the function by 
+// returning the string 'Hello World'
+function hello(){
+    // add code here
+    return 'Hello World'
+}
+
+// #2 Complete the function
+// by returning a + b
+function sum(a, b){
+    // add code here
+    return a+b 
+}
+
+// #3 Complete the function by returning
+// 'Hello ' + name
+function helloPerson(name){
+    // add code here
+    return 'Hello ' + name
+}
+
+// #4 Complete the function 
+// by returning a minus b
+function minus(a, b){
+    // add code here
+    return a - b 
+} 
+
+// #5 Complete the function 
+// by returning a divided by b
+function divide(a, b){
+  return  a/b
+    // add code here
+}
+
+
+module.exports = {hello, sum, helloPerson, minus, divide}
---
+++ practice/02_function_parameters.js
@@ -0,0 +1,28 @@
+
+// #1 Complete the function by adding the appropriate
+// parameters to the function.
+function double(num){ // <-- add code inside the parenthesis ()
+    return num * 2
+}
+
+// #2 Complete the function by adding the appropriate
+// parameters to the function.
+function multiply(a,b){ // <-- add code inside the parenthesis ()
+    return a * b;
+}
+
+// #3 Complete the function by adding the appropriate
+// parameters to the function.
+function shout(word){ // <-- add code inside the parenthesis ()
+    return word.toUpperCase();
+}
+
+// #4 Complete the function by adding the appropriate
+// parameters to the function.
+function first(arr){ // <-- add code inside the parenthesis ()
+    return arr[0];
+}
+
+
+
+module.exports = {double, multiply, shout, first}
